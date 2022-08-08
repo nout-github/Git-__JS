497425aa0002a459944beac214cc6e6e5023900c
izzatilla <izzatilla@gmail.com>
2022-08-08 15:07:40 +0500

    index.html

---
+++ script.js
@@ -0,0 +1,21 @@
+var str =prompt (' Ismingizni Kiriting ')
+console.info( ' Sizning Ismingiz   ' + str)
+
+var num = +prompt (' Yoshingizni Kiriting ')
+console.info( ' Sizning Yoshingiz  ' + num)
+alert(' Sizni Ismingiz : '     +                str +             '        Sizni Yoshingiz : ' + num)
+
+var num2 = +prompt(' Musolni Yeching 10 + 5')
+var num3 = +prompt(' Musolni Yeching 10 - 5')
+var num4 = +prompt(' Musolni Yeching 10 % 5')
+var num5 = +prompt(' Musolni Yeching 10 * 5')
+var num6 = +prompt(' Musolni Yeching 10 / 5')
+
+console.info( '  10 + 5 = ' + num2 + '  Togri Jovob 15')
+console.info( '  10 - 5 = ' + num3 + '  Togri Jovob 5')
+console.info( '  10 % 5 = ' + num4 + '  Togri Jovob 0')
+console.info( '  10 * 5 = ' + num5 + '  Togri Jovob 50')
+console.info( '  10 / 5 = ' + num6 + '  Togri Jovob 2')
+
+
+alert(' CONSOLE ga Oting')
